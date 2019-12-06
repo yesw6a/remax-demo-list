0373a333682ace13a2b0843e7eb2910492e428cc
w6a <583783099@qq.com>
2019-12-06 11:31:55 +0800

    feat: 测试v1.7feat点击阻止冒泡回调事件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "remax/wechat";
+import { View, Text, Button, showModal } from "remax/wechat";
 import styles from "./index.module.css";
 import Region from "../Region";
 import Charts from "../Charts";
@@ -7,10 +7,41 @@ import Charts from "../Charts";
 export default () => {
   return (
     <View className={styles.app}>
-      <Text>====== Region ======</Text>
+      <Text className={styles.title}>====== Region ======</Text>
       <Region />
-      <Text>====== Charts ======</Text>
+      <Text className={styles.title}>====== Charts ======</Text>
       <Charts />
+      <Text className={styles.title}>====== Button ======</Text>
+      <Text className={styles.tips}>
+        测试在绝对定位情况下按钮的阻止冒泡回调事件(stopPropagation)
+      </Text>
+      <View
+        style={{
+          position: "relative",
+          width: "500rpx",
+          height: "300rpx",
+          backgroundColor: "#000"
+        }}
+        onClick={() => {
+          showModal({ title: "view click" });
+        }}
+      >
+        <Button
+          style={{
+            position: "absolute",
+            top: "20rpx",
+            left: "20rpx",
+            width: "300rpx",
+            height: "100rpx"
+          }}
+          onClick={e => {
+            e.stopPropagation();
+            showModal({ title: "btn click" });
+          }}
+        >
+          Button
+        </Button>
+      </View>
     </View>
   );
 };
